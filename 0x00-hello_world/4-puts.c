4b59d9314f1d8c00b211b22c9dc76f83aae192d4
AyzCoded <meetprinceay@gmail.com>
2022-12-08 12:16:52 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/*
+ * Program Author: Agbe Ayodele (AyzCoded)
+ * file: the program is written in theC language
+ */
+
+#include <stdio.h>
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+
+
+}
