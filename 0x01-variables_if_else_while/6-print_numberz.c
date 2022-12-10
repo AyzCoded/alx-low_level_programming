ef2d4ed3effd44603ce8cf38eb2e0ba8d0f4c721
AyzCoded <meetprinceay@gmail.com>
2022-12-10 10:51:18 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - code block
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int num;
+for (num = '0'; num <= '9'; num++)
+{
+putchar(n);
+}
+putchar('\n');
+}
+return (0)
+}
