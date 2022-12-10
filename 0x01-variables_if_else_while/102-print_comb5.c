be4499cf45fb93674378744fba32e86912c2294c
AyzCoded <meetprinceay@gmail.com>
2022-12-09 16:39:29 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,7 +14,9 @@
 int main(void)
 {
 int num1, num2;
+
 for (num1 = 0; num1 <= 98; num1++);
+
 {
 for (num2 = num1 + 1; num2 <= 99; num2++)
 {
@@ -23,7 +25,10 @@ putchar ((num1 % 10) + '0');
 putchar (' ');
 putchar ((num2 / 10) + '0');
 putchar ((num2 / 10) + '0');
-if (num1 == 98 && num2 == 99) continue;
+if (num1 == 98 && num2 == 99)
+
+continue;
+
 putchar (',');
 putchar (' ');
 }
