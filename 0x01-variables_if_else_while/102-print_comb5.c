00ce4ff5e05fdbfde3268f9c3c7ca3ed782a435b
AyzCoded <meetprinceay@gmail.com>
2022-12-11 18:13:35 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,25 +9,24 @@
 
 int main(void)
 {
-int i , j;
+int i, j;
 for (j = 0; j <= 100; j++)
 {
 for (i = j; i <= 100; i++)
 {
-  if (i > j)
+if (i > j)
 {
 putchar(j / 10 + 48);
 putchar(j % 10 + 48);
 putchar(' ');
 putchar(i / 10 + 48);
 putchar(i % 10 + 48);
-}
 if (j != 98 || i != 99)
 {
 putchar(',');
 putchar(' ');
 }
-}
+
 }
 putchar('\n');
 return (0);
