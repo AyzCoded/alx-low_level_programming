169eb4be2459f91e2f567b2817044a7cfb9990bc
AyzCoded <meetprinceay@gmail.com>
2022-12-14 06:32:55 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - Check Main
  * @c: An input character
- * Description:  function uses _putchar function to 
+ * Description:  function uses _putchar function to
  * print alphabet in lowercase 10 times
  * Return: 1 if is lowercase or 0 if is uppercase
  */
