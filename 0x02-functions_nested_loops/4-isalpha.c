adcedd2b8d52eca6d8952bc55099317fc8108164
AyzCoded <meetprinceay@gmail.com>
2022-12-13 20:39:34 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/** _isalpha - checks
+ *@c: The character to be cgecked
+ * Return: 1 if cgaracter is letter
+ */
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || ((c >= 'A' && c<= 'Z'))
+return (1);
+else
+return (0);
+}
