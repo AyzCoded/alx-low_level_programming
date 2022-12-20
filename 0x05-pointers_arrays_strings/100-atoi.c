a00b2b776e2ab0012e05756ae21a4d7598960717
AyzCoded <meetprinceay@gmail.com>
2022-12-19 18:31:28 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,11 +11,29 @@ int _atoi(char *s)
 int c = 0;
 unsigned int ni = 0;
 int min = 1;
-int isi = 0;
+int isint = 0;
 
 while (s[c])
 {
 if (s[c] == 45)
 {
 min *= -1;
+}
+while (s[c] >= 48 && s[c] <= 57)
+{
+isint = 1;
+ni = (ni * 10) + (s[c] - '0');
+c++;
+}
+
+if (isint == 1)
+{
+break;
+}
+
+c++;
+}
+
+ni *= min;
+return (ni);
 }
