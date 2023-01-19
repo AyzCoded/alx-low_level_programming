10003f3d770baa2a39f69335c6f2a65fb154be14
AyzCoded <meetprinceay@gmail.com>
2023-01-18 20:22:41 -0800

    3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* op_add - ayz
+* @a: ayz
+* @b: ayz
+*
+* Return: ayz
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+* op_sub - ayz
+* @a: ayz
+* @b: ayz
+*
+* Return: ayz
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+*op_mul - ayz
+* @a: ayz
+* @b: ayz
+*
+* Return: ayz
+*/
