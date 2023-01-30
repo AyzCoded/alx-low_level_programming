a97a83147ca59e8705f31a858f07ace8144b3c34
AyzCoded <meetprinceay@gmail.com>
2023-01-30 12:50:06 -0800

    #include lists.h /** * reverse_listint - reverses a listint_t linked list * @head: double pointer to head * Return: address of head of list */
    listint_t *reverse_listint(listint_t **head)
    {
    listint_t *p = NULL, *n = NULL;
    
    {
    return (*head);
    }
    n = (*head)->next;
    
    while (n->next)
    {
    n = (*head)->next;
    (*head)->next = p;
    p = *head;
    *head = n;
    }
    if (n)
    {
    n->next = p;
    }
    return (*head);
    }

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,28 +1,30 @@
 #include "lists.h"
-
 /**
-* reverse_listint - the function that reverses a listint_t
-* @head: double pointer
-*
-* Return: a pointer to the first node of the reversed list
+* reverse_listint - reverses a listint_t linked list
+* @head: double pointer to head
+* Return: address of head of list
 */
 
 listint_t *reverse_listint(listint_t **head)
 {
-listint_t *previous, *next;
+listint_t *p = NULL, *n = NULL;
 
-if (head == NULL || *head == NULL)
-return (NULL);
-if ((*head)->next == NULL)
-return (*head);
-previous = NULL;
-while (*head != NULL)
+if(!head || !(*head))
 {
-next = (*head)->next;
-(*head)->next = previous;
-previous = *head;
-*head = next;
+return (*head);
 }
-*head = previous;
+n = (*head)->next;
+
+while (n->next)
+{
+n = (*head)->next;
+(*head)->next = p;
+p = *head;
+*head = n;
+}
+if (n)
+{
+n->next = p;
+}
 return (*head);
 }
